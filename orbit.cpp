f8e45545d5730922219afb05dba8b2ab1fa4c14e
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-10-01 13:37:47 +0100

    Update orbit.cpp

--- orbit.cpp
+++ orbit.cpp
@@ -5,29 +5,36 @@
 #include <GL/glext.h>
 static GLfloat spin = 0.0;
 void init(void){
-	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glShadeModel (GL_FLAT);
 }
 void display(void)
 {
+	glClearColor (0.110, 0.183, 0.255, 1.0);
     glClear(GL_COLOR_BUFFER_BIT); 
     glPushMatrix();
     
     glRotatef(90.0,1.0,0.0,0.0);
 	glRotatef(spin, 0.0, 0.0, 1.0);
     
+    //sun
     glColor3ub(255,255,255);
     glutWireSphere(0.3,20,20);
     
+    //earth
     glTranslatef(.55, 0, 0);
-    glColor3ub(120,120,30);
+    glColor3ub(255,255,255);
 	glutWireSphere(0.1,20,20);
+	
+	//moon
+	glTranslatef(.2, 0, 0);
+    glColor3ub(255,255,255);
+	glutWireSphere(0.05,20,20);
     
     glPopMatrix();
 	glutSwapBuffers();
 }
-void spinDisplay(void)
-{
+
+void spinDisplay(void){
 	spin = spin + 1.0;
 	if (spin > 360.0)
 		spin = spin - 360.0;
